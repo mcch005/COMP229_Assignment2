382c1bf1b8c8d3cd6282c22593b5d72a420a6c27
mcch005 <martinchanch01@gmail.com>
2023-06-21 13:13:28 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,6 @@
+// module.exports = {
+//     url:"mongodb+srv://martinchanch01:ACaejyulZlPPh5Fr@cluster0.pgiah6v.mongodb.net/SportStore?retryWrites=true&w=majority"
+// };
+module.exports = {
+    url: "mongodb+srv://martinchanch01:znXhC27Qhzw1pJ6e@cluster0.ymzieob.mongodb.net/DressStore?retryWrites=true&w=majority"    
+};
